41ddd4410c9c94779ece9a8b64ac82f1b10c3c2e
Noah <179856882+Noah-Johann@users.noreply.github.com>
2024-12-10 14:37:00 +0100

    print() hinzugefügt

--- ampel_button.py
+++ ampel_button.py
@@ -18,22 +18,30 @@ def traffic_light():
             GRUEN.on()
 
             if button.is_pressed:
+                print("button pressed")
+                sleep(1)
+
                 #Gelbphase
+                print("gelb")
                 GRUEN.off()
                 GELB.on()
                 sleep(1.5)
 
                 #Rotphase
+                print("rot")
                 ROT.on()
                 GELB.off()
 
                 #Fußgängerampel
+                print("blau")
                 for __ in range(14):
                     BLAU.on()
                     sleep(0.5)
                     BLAU.off()
                     sleep(0.5)
-                
+
+                print("rotgelb")
+                ROT.on()
                 GELB.on()
                 sleep(2.5)
 
